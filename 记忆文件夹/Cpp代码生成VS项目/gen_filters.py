d0c7e43b5435ed8285fcbc98ea5044a9c9d3a516
Richard <wzc_0618@126.com>
2019-09-11 20:39:56 +0800

    增加对配置文件以及makefile的支持

--- 记忆文件夹/Cpp代码生成VS项目/gen_filters.py
+++ 记忆文件夹/Cpp代码生成VS项目/gen_filters.py
@@ -7,6 +7,7 @@ headers = []
 cpp_files    = []
 dirs = []
 filters_file_name = ""
+other_files = [] # .conf makefile
 def get_file_path(root_path,file_list,dir_list,start):
     #获取该目录下所有的文件名称和目录名称
     dir_or_files = os.listdir(root_path)
@@ -23,32 +24,34 @@ def get_file_path(root_path,file_list,dir_list,start):
         else:
             file_list.append(dir_file_path)
             project_file_name = dir_file_path[start:]
-            valid_cpp_types = ["cpp","c","cc","cxx"]
-            valid_header_types = ["h","hh","hpp","hxx"]
+            valid_cpp_types = ["cpp","c","cc","cxx"]        # 源文件类型
+            valid_header_types = ["h","hh","hpp","hxx"]     #头文件类型
+            other_types = ["makefile","conf","tars"]        # 其它文件类型
             valid_ext_type = valid_cpp_types + valid_header_types
             for t in valid_ext_type:
                 cmd = ".*\." + t + "$"
                 pattern = re.compile(cmd)
-                # print("cmd:" + cmd)
                 if pattern.match(project_file_name):
                     if t in valid_header_types:
                         headers.append(project_file_name)
                     else:
                         cpp_files.append(project_file_name)
-                # if t in project_file_name:
-                #     print(project_file_name)
-                #
-                #     if ".cpp" in project_file_name:
-                #         cpp_files.append(project_file_name)
-                #     else:
-                #         headers.append(project_file_name)
                     vs_filter_end = str(project_file_name).rfind("\\")
                     if vs_filter_end != -1:
                         dir_name = project_file_name[0 :vs_filter_end]
                         if dir_name not in dirs:
                             dirs.append(dir_name)
-                        # print(project_file_name[0 :vs_filter_end])
-
+            # conf 和makefileif  和 tars
+            for t in other_types:
+                cmd = ".*" + t + "$"
+                pattern = re.compile(cmd)
+                if pattern.match(project_file_name):
+                    other_files.append(project_file_name)
+                    vs_filter_end = str(project_file_name).rfind("\\")
+                    if vs_filter_end != -1:
+                        dir_name = project_file_name[0 :vs_filter_end]
+                        if dir_name not in dirs:
+                            dirs.append(dir_name)
 
 def get_unique_id():
     s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzyx"
@@ -65,7 +68,7 @@ def create_dir(dir_names):
     for dir_name in dir_names:
         msg += '<Filter Include="' + dir_name + '">\n'
         msg += '<UniqueIdentifier>{' + get_unique_id() + '}</UniqueIdentifier>\n'
-        msg += '</Filter>'
+        msg += '</Filter>\n'
     msg +='</ItemGroup>\n'
     return msg
 
@@ -98,6 +101,18 @@ def create_header_item_group(header_file_names):
     msg += '</ItemGroup>\n'
     return msg
 
+def create_other_item_group(other_file_names):
+    msg = '<ItemGroup>\n'
+    for file_name in other_file_names:
+        idx = str(file_name).rfind('\\')
+        if idx == -1:
+            msg += '<None Include="' + file_name + '" />\n'
+        else:
+            msg += '<None Include="' + file_name + '">\n'
+            msg += '<Filter>' + file_name[0:idx] + '</Filter>\n'
+            msg += '</None>\n'
+    msg += '</ItemGroup>\n'
+    return msg
 
 def get_filters_name(root_path):
     dir_or_files = os.listdir(root_path)
@@ -120,6 +135,7 @@ def create_vs_filters(dirs,cpp_file_names,header_file_names):
     msg += create_dir(dirs)
     msg += create_cpp_item_group(cpp_file_names)
     msg += create_header_item_group(header_file_names)
+    msg += create_other_item_group(other_files)
     msg += '</Project>'
 
     file.write(msg)
--- 记忆文件夹/Cpp代码生成VS项目/gen_vcsproj.py
+++ 记忆文件夹/Cpp代码生成VS项目/gen_vcsproj.py
@@ -3,6 +3,7 @@ import os
 headers = []
 cpp_files    = []
 dirs = []
+other_files = [] # .conf makefile
 def get_file_path(root_path,file_list,dir_list,start):
     #获取该目录下所有的文件名称和目录名称
     dir_or_files = os.listdir(root_path)
@@ -21,6 +22,7 @@ def get_file_path(root_path,file_list,dir_list,start):
             project_file_name = dir_file_path[start:]
             valid_cpp_types = ["cpp","c","cc","cxx"]
             valid_header_types = ["h","hh","hpp","hxx"]
+            other_types = ["makefile","conf","tars"]        # 其它文件类型
             valid_ext_type = valid_cpp_types + valid_header_types
             for t in valid_ext_type:
                 cmd = ".*\." + t + "$"
@@ -44,6 +46,17 @@ def get_file_path(root_path,file_list,dir_list,start):
                         if dir_name not in dirs:
                             dirs.append(dir_name)
                         # print(project_file_name[0 :vs_filter_end])
+            # conf 和makefileif  和 tars
+            for t in other_types:
+                cmd = ".*" + t + "$"
+                pattern = re.compile(cmd)
+                if pattern.match(project_file_name):
+                    other_files.append(project_file_name)
+                    vs_filter_end = str(project_file_name).rfind("\\")
+                    if vs_filter_end != -1:
+                        dir_name = project_file_name[0 :vs_filter_end]
+                        if dir_name not in dirs:
+                            dirs.append(dir_name)
 
 
 def create_cpp_item_group(cpp_file_names):
@@ -57,6 +70,18 @@ def create_header_item_group(header_file_names):
     msg = '<ItemGroup>\n'
     for file_name in header_file_names:
         msg += '\t<ClInclude Include="' + file_name + '" />\n'
+    msg += '</ItemGroup>\n'
+    return msg
+def create_other_item_group(other_file_names):
+    msg = '<ItemGroup>\n'
+    for file_name in other_file_names:
+        idx = str(file_name).rfind('\\')
+        if idx == -1:
+            msg += '<None Include="' + file_name + '" />\n'
+        else:
+            msg += '<None Include="' + file_name + '">\n'
+            msg += '<Filter>' + file_name[0:idx] + '</Filter>\n'
+            msg += '</None>\n'
     msg += '</ItemGroup>\n'
     return msg
 
@@ -103,7 +128,8 @@ for i in range(len(lines)):
 
 cpp_items = create_cpp_item_group(cpp_files)
 headers_items = create_header_item_group(headers)
-lines = lines[0:item_group_start] + [cpp_items,headers_items] + lines[item_group_end+1:]
+other_items = create_other_item_group(other_files)
+lines = lines[0:item_group_start] + [cpp_items,headers_items, other_items] + lines[item_group_end+1:]
 print(''.join(lines))
 
 # 写入文件
