e4ab61d857d0586b357b5bfd5e516a0bc89ac40a
richie <861949775@qq.com>
2020-08-05 14:59:25 +0800

    同时配置多个反向代理

--- 记忆文件夹/nginx核心知识100讲/nginx安装.sh
+++ 记忆文件夹/nginx核心知识100讲/nginx安装.sh
@@ -33,15 +33,28 @@ make install
 # configure 时 加上--with-stream这个参数，以加载ngx_stream_core_module这个模块
 # 监听本机的2222端口，实现跳转到192.168.56.12的22号端口
 stream {
-upstream tcp_proxy{
-hash $remote_addr consistent;
-server 192.168.56.12:22;
+    upstream tcp_proxy{
+    hash $remote_addr consistent;
+    server 178.128.61.189:8399;
+    }
+
+    server {
+    listen 8399 so_keepalive=on;
+    proxy_connect_timeout 10s;
+    proxy_timeout 30s;
+    proxy_pass tcp_proxy;
+    }
+
+    upstream tcp_proxy_2{
+    hash $remote_addr consistent;
+    server 178.128.61.189:8306;
+    }
+
+    server {
+    listen 8306 so_keepalive=on;
+    proxy_connect_timeout 10s;
+    proxy_timeout 30s;
+    proxy_pass tcp_proxy_2;
+    }
 }
 
-server {
-listen 2222 so_keepalive=on;
-proxy_connect_timeout 1s;
-proxy_timeout 3s;
-proxy_pass tcp_proxy;
-}
-}
